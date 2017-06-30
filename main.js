0ea4a6b601c20f54936431120f55329429a05c3a
Shehan Atukorala <ultimateweapon432@gmail.com>
2017-06-30 12:48:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+    $.ajax({
+    type:"POST",
+    $("#searching").onClick(
+    var searchWord = $("searcher").val();
+    );
+
+url:"https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchWord +'&format=jsonfm",
+    success: function(response){
+    console.log(response);
+    },
+    });
+
+});
