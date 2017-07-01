2e959f17447c06225caceb76a8469ae848f15820
Shehan Atukorala <ultimateweapon432@gmail.com>
2017-07-01 14:44:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 ﻿$(document).ready(function(){
-    $.ajax({
-    type:"POST",
-    $("#searching").onClick(
-    var searchWord = $("searcher").val();
-    );
-
-url:"https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchWord +'&format=jsonfm",
-    success: function(response){
-    console.log(response);
-    },
+    $("#search").on("click", function(){
+    var searchWord = $("#searcher").val();
+     $.ajax({
+     type:"GET",
+     url:"https://en.wikipedia.org/w/api.php?action=opensearch&search='+ searchWord+'&format=json",
+     dataType:"json",
+     success: function(data){
+         $("#output1").html("");
+         for (var i = 0; i< data[1].length; i++){
+        $("#output1").prepend("<li><a href='"+data[3][i]+"'>'"+data[1][i]+"'</a><p>'"+data[2][i]+"'</p></li>"); 
+             
+     }}
+     error: function(errorMessage){
+        alert("An error occured, Please try again later");
+     }
+     });//ajax request ends
+    
     });
-
+   
 });
